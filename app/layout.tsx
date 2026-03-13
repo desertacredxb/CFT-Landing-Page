c58d5b4f601ddc5d81562d5821a9e3f3a2da250e
Anshu-WebD <anshuweb@bigwigmediadigital.com>
2026-03-13 16:24:26 +0530

    updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-import Script from "next/script";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
@@ -29,20 +28,6 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-[var(--cft-bg-dark)] text-[var(--cft-text-main)]`}
       >
         {children}
-
-        {/* Zoho SalesIQ Live Chat */}
-        <Script id="zoho-salesiq-init" strategy="afterInteractive">
-          {`
-            window.$zoho=window.$zoho || {};
-            $zoho.salesiq=$zoho.salesiq||{ready:function(){}};
-          `}
-        </Script>
-
-        <Script
-          id="zoho-salesiq-script"
-          src="https://salesiq.zohopublic.in/widget?wc=siq4c8e819cb495c0e3a7681477896023e14714babb3b73f6f9e32d866f47fce36b"
-          strategy="afterInteractive"
-        />
       </body>
     </html>
   );
