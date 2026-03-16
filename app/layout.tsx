1b7267d1f372e2536caec57627777616bc8c0449
Anshu-WebD <anshuweb@bigwigmediadigital.com>
2026-03-16 13:23:40 +0530

    updated gtm tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,6 +25,23 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        {/* Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-C7VV3G2FM7"
+          strategy="afterInteractive"
+        />
+
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-C7VV3G2FM7');
+          `}
+        </Script>
+      </head>
+
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-[var(--cft-bg-dark)] text-[var(--cft-text-main)]`}
       >
