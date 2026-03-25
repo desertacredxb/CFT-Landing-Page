534ea78bd7f613dbd286e409401fed6f386527b5
Anshu-WebD <anshuweb@bigwigmediadigital.com>
2026-03-25 16:33:07 +0530

    updated again

--- app/component/Navbar.tsx
+++ app/component/Navbar.tsx
@@ -24,7 +24,7 @@ export default function Navbar() {
     <motion.header
       initial={{ y: -80, opacity: 0 }}
       animate={{ y: 0, opacity: 1 }}
-      transition={{ duration: 0.6 }}
+      transition={{ duration: 0.5 }}
       className="fixed top-0 w-full z-50 flex justify-between items-center px-6 md:px-28 py-6"
     >
       {/* Logo */}
